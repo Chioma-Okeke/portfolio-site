1c2735c7f6e67ce5015dc03ac81273304c56213f
chioma-okeke <okekegeraldenne@gmail.com>
2024-07-08 19:08:02 +0100

    Set up all contact forms and every message is savedin a google sheet

--- src/components/HireMeModal.jsx
+++ src/components/HireMeModal.jsx
@@ -1,111 +1,122 @@
-import { motion } from 'framer-motion';
-import { FiX } from 'react-icons/fi';
-import Button from './reusable/Button';
-import { useState } from 'react';
+import { motion } from "framer-motion";
+import { FiX } from "react-icons/fi";
+import Button from "./reusable/Button";
+import { useState } from "react";
+import axios from "axios";
 
 const HireMeModal = ({ onClose, onRequest }) => {
-	const [formData, setFormData] = useState({})
+    const [formData, setFormData] = useState({});
 
-	function handleChange (e) {
-		const {name, value }= e.target
-		console.log(name, value)
-		console.log(e.target.value)
-		setFormData(prevState => {
-			return {
-				...prevState,
-				[name]: value	
-			}
-		})
-	}
+    function handleChange(e) {
+        const { name, value } = e.target;
+        console.log(name, value);
+        console.log(e.target.value);
+        setFormData((prevState) => {
+            return {
+                ...prevState,
+                [name]: value,
+            };
+        });
+    }
 
-	function submitForm(e) {
-		console.log(formData)
-		e.preventDefault()
-		fetch("/macros/s/AKfycbyIR8cqRC4NhFNHEEfSHVtC2-SOsAXWS6iRRNADgd6z_YdyUgfQJbmeLELrNhNjYAsfjw/exec", {
-			method: "POST",
-			body: formData
-		}).then((res) => res.json()).then((data)=>{
-			console.log(data)
-		}).catch((err) => console.log(err))
-	}
+    function submitForm(e) {
+        console.log(formData);
+        e.preventDefault();
+		e.target.reset()
+        axios
+            .post(
+                "https://sheet.best/api/sheets/11470b3e-3931-4af6-aeec-2187674e3400",
+                formData
+            )
+            .then((res) => {
+				setFormData(prevState => {
+					return {
+						...prevState,
+						Email: "",
+						Name: "",
+						Message: ""
+					}
+				})
+			})
+    }
 
-	return (
-		<motion.div
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
-			className="font-general-medium fixed inset-0 z-30 transition-all duration-500"
-		>
-			{/* Modal Backdrop */}
-			<div className="bg-filter bg-black bg-opacity-50 fixed inset-0 w-full h-full z-20"></div>
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="font-general-medium fixed inset-0 z-30 transition-all duration-500"
+        >
+            {/* Modal Backdrop */}
+            <div className="bg-filter bg-black bg-opacity-50 fixed inset-0 w-full h-full z-20"></div>
 
-			{/* Modal Content */}
-			<main className="flex flex-col items-center justify-center h-full w-full">
-				<div className="modal-wrapper flex items-center z-30">
-					<div className="modal max-w-md mx-5 xl:max-w-xl lg:max-w-xl md:max-w-xl bg-secondary-light dark:bg-primary-dark max-h-screen shadow-lg flex-row rounded-lg relative">
-						<div className="modal-header flex justify-between gap-10 p-5 border-b border-ternary-light dark:border-ternary-dark">
-							<h5 className=" text-primary-dark dark:text-primary-light text-xl">
-								What project are you looking for?
-							</h5>
-							<button
-								onClick={onClose}
-								className="px-4 font-bold text-primary-dark dark:text-primary-light"
-							>
-								<FiX className="text-3xl" />
-							</button>
-						</div>
-						<div className="modal-body p-5 w-full h-full">
-							<form
-								onSubmit={(e) => {
-									submitForm(e)
-								}}
-								className="max-w-xl m-4 text-left"
-							>
-								<div className="">
-									<input
-										className="w-full px-5 py-2 border dark:border-secondary-dark rounded-md text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light"
-										id="name"
-										name="name"
-										type="text"
-										value={formData.name}
-										required=""
-										placeholder="Name"
-										aria-label="Name"
-										onChange={(e) => handleChange(e)}
-									/>
-								</div>
-								<div className="mt-6">
-									<input
-										className="w-full px-5 py-2 border dark:border-secondary-dark rounded-md text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light"
-										id="email"
-										name="email"
-										type="text"
-										value={formData.email}
-										required=""
-										placeholder="Email"
-										aria-label="Email"
-										onChange={(e) => handleChange(e)}
-									/>
-								</div>
+            {/* Modal Content */}
+            <main className="flex flex-col items-center justify-center h-full w-full">
+                <div className="modal-wrapper flex items-center z-30">
+                    <div className="modal max-w-md mx-5 xl:max-w-xl lg:max-w-xl md:max-w-xl bg-secondary-light dark:bg-primary-dark max-h-screen shadow-lg flex-row rounded-lg relative">
+                        <div className="modal-header flex justify-between gap-10 p-5 border-b border-ternary-light dark:border-ternary-dark">
+                            <h5 className=" text-primary-dark dark:text-primary-light text-xl">
+                                What project are you looking for?
+                            </h5>
+                            <button
+                                onClick={onClose}
+                                className="px-4 font-bold text-primary-dark dark:text-primary-light"
+                            >
+                                <FiX className="text-3xl" />
+                            </button>
+                        </div>
+                        <div className="modal-body p-5 w-full h-full">
+                            <form
+                                onSubmit={(e) => {
+                                    submitForm(e);
+                                }}
+                                className="max-w-xl m-4 text-left"
+                            >
+                                <div className="">
+                                    <input
+                                        className="w-full px-5 py-2 border dark:border-secondary-dark rounded-md text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light"
+                                        id="Name"
+                                        name="Name"
+                                        type="text"
+                                        value={formData.name}
+                                        required=""
+                                        placeholder="Name"
+                                        aria-label="Name"
+                                        onChange={(e) => handleChange(e)}
+                                    />
+                                </div>
+                                <div className="mt-6">
+                                    <input
+                                        className="w-full px-5 py-2 border dark:border-secondary-dark rounded-md text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light"
+                                        id="Email"
+                                        name="Email"
+                                        type="text"
+                                        value={formData.email}
+                                        required=""
+                                        placeholder="Email"
+                                        aria-label="Email"
+                                        onChange={(e) => handleChange(e)}
+                                    />
+                                </div>
 
-								<div className="mt-6">
-									<textarea
-										className="w-full px-5 py-2 border dark:border-secondary-dark rounded-md text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light"
-										id="message"
-										name="message"
-										cols="14"
-										rows="6"
-										value={formData.message}
-										aria-label="Details"
-										placeholder="Project description"
-										onChange={(e) => handleChange(e)}
-									></textarea>
-								</div>
+                                <div className="mt-6">
+                                    <textarea
+                                        className="w-full px-5 py-2 border dark:border-secondary-dark rounded-md text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light"
+                                        id="Message"
+                                        name="Message"
+                                        cols="14"
+                                        rows="6"
+                                        value={formData.message}
+                                        aria-label="Details"
+                                        placeholder="Project description"
+                                        onChange={(e) => handleChange(e)}
+                                    ></textarea>
+                                </div>
 
-								<div className="mt-6 pb-4 sm:pb-1">
-									<span
-										type="submit"
-										className="px-4
+                                <div className="mt-6 pb-4 sm:pb-1">
+                                    <span
+                                        type="submit"
+                                        className="px-4
 											sm:px-6
 											py-2
 											sm:py-2.5
@@ -114,32 +125,32 @@ const HireMeModal = ({ onClose, onRequest }) => {
 											hover:bg-indigo-600
 											rounded-md
 											focus:ring-1 focus:ring-indigo-900 duration-500"
-										aria-label="Submit Request"
-									>
-										<Button title="Send Request" />
-									</span>
-								</div>
-							</form>
-						</div>
-						<div className="modal-footer mt-2 sm:mt-0 py-5 px-8 border0-t text-right">
-							<span
-								onClick={onClose}
-								type="button"
-								className="px-4
+                                        aria-label="Submit Request"
+                                    >
+                                        <Button title="Send Request" />
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer mt-2 sm:mt-0 py-5 px-8 border0-t text-right">
+                            <span
+                                onClick={onClose}
+                                type="button"
+                                className="px-4
 									sm:px-6
 									py-2 bg-gray-600 text-primary-light hover:bg-ternary-dark dark:bg-gray-200 dark:text-secondary-dark dark:hover:bg-primary-light
 									rounded-md
 									focus:ring-1 focus:ring-indigo-900 duration-500"
-								aria-label="Close Modal"
-							>
-								<Button title="Close" />
-							</span>
-						</div>
-					</div>
-				</div>
-			</main>
-		</motion.div>
-	);
+                                aria-label="Close Modal"
+                            >
+                                <Button title="Close" />
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </motion.div>
+    );
 };
 
 export default HireMeModal;
--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import Button from '../reusable/Button';
 import FormInput from '../reusable/FormInput';
+import axios from 'axios';
 
 const ContactForm = () => {
+	const [formData, setFormData] = useState({})
+
+	function handleChange(e) {
+		const {name, value} = e.target
+		setFormData(prevState => {
+			return {
+				...prevState,
+				[name]: value
+			}
+		})
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		e.target.reset()
+		axios
+		.post(
+			"https://sheet.best/api/sheets/11470b3e-3931-4af6-aeec-2187674e3400",
+			formData
+		).then(res => console.log(res))
+	}
+
 	return (
 		<div className="w-full lg:w-1/2">
 			<div className="leading-loose">
 				<form
 					onSubmit={(e) => {
-						e.preventDefault();
+						handleSubmit(e)
 					}}
 					className="max-w-xl m-4 p-6 sm:p-10 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left"
 				>
@@ -16,30 +40,36 @@ const ContactForm = () => {
 					</p>
 					<FormInput
 						inputLabel="Full Name"
-						labelFor="name"
+						labelFor="Name"
 						inputType="text"
-						inputId="name"
-						inputName="name"
+						inputId="Name"
+						inputName="Name"
 						placeholderText="Your Name"
 						ariaLabelName="Name"
+						inputValue={formData.Name}
+						onChange={(e) => handleChange(e)}
 					/>
 					<FormInput
 						inputLabel="Email"
-						labelFor="email"
+						labelFor="Email"
 						inputType="email"
-						inputId="email"
-						inputName="email"
+						inputId="Email"
+						inputName="Email"
 						placeholderText="Your email"
 						ariaLabelName="Email"
+						inputValue={formData.Email}
+						onChange={(e) => handleChange(e)}
 					/>
 					<FormInput
 						inputLabel="Subject"
-						labelFor="subject"
+						labelFor="Subject"
 						inputType="text"
-						inputId="subject"
-						inputName="subject"
+						inputId="Subject"
+						inputName="Subject"
 						placeholderText="Subject"
 						ariaLabelName="Subject"
+						inputValue={formData.Subject}
+						onChange={(e) => handleChange(e)}
 					/>
 
 					<div className="mt-6">
@@ -51,11 +81,13 @@ const ContactForm = () => {
 						</label>
 						<textarea
 							className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
-							id="message"
-							name="message"
+							id="Message"
+							name="Message"
 							cols="14"
 							rows="6"
 							aria-label="Message"
+							inputValue={formData.Message}
+							onChange={(e) => handleChange(e)}
 						></textarea>
 					</div>
 
